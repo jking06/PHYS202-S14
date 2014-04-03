6b0b9f94bd19634faac25ea881a6ca5eafbae825
Jonathan King <jking06@calpoly.edu>
2014-04-03 13:05:15 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script configures git whenever you start working
+# on multiple machines. Run it any time you decide to work
+# with git on a new computer
+
 git config --global user.name "Jonathan King"
 git config --global user.email "jking06@calpoly.edu"
 git config --global colors.ui "auto"
