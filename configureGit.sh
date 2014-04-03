40dd0a1b869116c7ea3d1be0f5b2b75264e4de55
Jonathan King <jking06@calpoly.edu>
2014-04-03 12:54:32 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Jonathan King"
+git config --global user.email "jking06@calpoly.edu"
+git config --global colors.ui "auto"
+git config --global core.editor "nano"
